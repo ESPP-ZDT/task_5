a18e87e4c2153c6ce2a4461cb4bedc9152651290
Twoje Imię Nazwisko <toplelfaget@gmail.com>
2023-01-10 20:40:50 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Route, Routes, Link } from "react-router-dom"
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
